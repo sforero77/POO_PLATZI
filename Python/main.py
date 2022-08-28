a7ad9d58bf17470c0f111437597b8c8f29c22fc2
sforero77 <54457059+sforero77@users.noreply.github.com>
2022-08-28 16:06:40 -0500

    constructor python

--- Python/main.py
+++ Python/main.py
@@ -1,16 +1,10 @@
-from ast import If
-
-
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
-    print("hola mundo")
-    car = Car()
-    car.license = "AMS234"
-    car.driver = "Andres Herrera"
+    print("Hola Mundo")
+    
+    car = Car("AMS234", Account("Andres Herrera", "ANDA876"))
     print(vars(car))
+    print(vars(car.driver))
 
-    car2 = Car()
-    car2.license = "QWE567"
-    car2.driver = "Martha"
-    print(vars(car2))
